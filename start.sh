9d14917d427d689d384d5fdd34b69b7272e3bc79
Dominik Dorn <dominik.dorn@gmail.com>
2011-06-17 16:12:44 +0200

    made dumbster running standalone

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
-sudo rm -R /root/.m2
-sudo ln -s /home/domdorn/.m2 /root/.m2
-sudo mvn exec:java -Dexec.mainClass=MailServer -o
+mvn clean compile package
+sudo java -jar target/dumbster-console-1.0-full.jar
